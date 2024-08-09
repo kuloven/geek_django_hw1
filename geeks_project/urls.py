7155e3256bcf36bf280e64aecbbf79f0be83a84b
kuloven <zhalikulovarlen@gmail.com>
2024-08-09 18:16:26 +0600

    update_homework1

--- geeks_project/urls.py
+++ geeks_project/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for DjangoMonth4 project.
+URL configuration for geeks_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from posts.views import word, main_page
+from posts.views import hello_world, main_page_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('opros/', word),
-    path('main/', main_page)
+    path('hello_world/', hello_world),
+    path('main/', main_page_view),
 ]
+
+
